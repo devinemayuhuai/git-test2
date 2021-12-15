a1db59182f70befbd4ca31110d41cae9a66771a1
myh <1513673369@qq.com>
2021-12-15 17:10:48 +0800

    git-master-4.0

--- src/main/java/com/myh/gitTest.java
+++ src/main/java/com/myh/gitTest.java
@@ -8,5 +8,6 @@ public class gitTest
         System.out.println("hello Git-hotfix1.0");
         System.out.println("master test");
         System.out.println("hello hot-fix-test");
+        System.out.println("push test");
     }
 }
