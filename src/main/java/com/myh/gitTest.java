49120807ef59bf7f0f0051bf6a9ddf709c48962a
马玉怀 <1513673369@qq.com>
2021-12-15 14:17:12 +0000

    update src/main/java/com/myh/gitTest.java.

--- src/main/java/com/myh/gitTest.java
+++ src/main/java/com/myh/gitTest.java
@@ -9,5 +9,6 @@ public class gitTest
         System.out.println("master test");
         System.out.println("hello hot-fix-test");
         System.out.println("push test");
+        System.out.println("gitee push test");
     }
 }
