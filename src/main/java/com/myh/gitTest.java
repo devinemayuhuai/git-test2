e30ee49b348b4e2f4a0fb20368b6d7f8c33959ec
myh <1513673369@qq.com>
2021-12-15 16:46:29 +0800

    git-test3.0

--- src/main/java/com/myh/gitTest.java
+++ src/main/java/com/myh/gitTest.java
@@ -5,5 +5,6 @@ public class gitTest
     public static void main(String[] args) {
         System.out.println("hello Git");
         System.out.println("hello Git2.0");
+        System.out.println("hello Git-hotfix1.0");
     }
 }
